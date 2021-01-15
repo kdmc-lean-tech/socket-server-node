0f5bfe27d9aa5872197bc79fa4e3a4bfc50b37ac
kevin <kevin.melan00@usc.edu.co>
2021-01-14 22:37:32 -0500

    Minor changes in index.html

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,16 @@
 const socket = io('https://socket-server-kevin.herokuapp.com/');
 
-const message = document.getElementById('message');
+const input = document.getElementById('input');
 const form = document.getElementById('form');
 const list = document.getElementById('list');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  const value = message.value;
+  const value = input.value;
   socket.emit('message-of-client', { message: value });
 });
 
 socket.on('message-of-server', ({ message }) => {
   list.innerHTML += `<li class="list-group-item">${ message }</li>`;
+  input.value = '';
 });
